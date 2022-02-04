ed35794d47d182a10e80fd56fb8905a3a1bd7608
naicop <nrombiola@gmail.com>
2022-02-04 18:33:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//Functions
+
+function changeClass() {
+  
+ 
+  document.querySelector("#active").className = "row-oferta-preaprobada-disable row";
+  document.querySelector("#disable").className = "row-oferta-preaprobada-active row";
+  
+  
+  
+}
+
+function changeClass2() {
+  
+ 
+  document.querySelector("#active").className = "row-oferta-preaprobada-active row";
+  document.querySelector("#disable").className = "row-oferta-preaprobada-disable row";
+  
+  
+  
+}
